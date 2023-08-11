0e189b5e720a6a620cea25b28db1561502386f95
AntonSokolovsky <anton.a.sokolovsky@gmail.com>
2023-08-11 23:30:40 +0300

    feat: add base classes  ElementCreator,View with types

---
+++ src/view/view.ts
@@ -0,0 +1,26 @@
+import { ElementParams, ViewParams } from '../type/params-element-type';
+import { ElementCreator } from '../utilities/element-creator';
+
+export default class View {
+  protected viewElementCreator:ElementCreator;
+
+  constructor(params: ViewParams = { tag: 'section', classNames: [] }) {
+    this.viewElementCreator = this.createView(params);
+  }
+
+  getHtmlElement():HTMLElement {
+    return this.viewElementCreator.getElement();
+  }
+
+  createView(params: ViewParams):ElementCreator {
+    const elementParams: ElementParams = {
+      tag: params.tag,
+      classNames: params.classNames,
+      textContent: '',
+      callback: null,
+    };
+    this.viewElementCreator = new ElementCreator(elementParams);
+
+    return this.viewElementCreator;
+  }
+}
