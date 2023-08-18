921d5c37948119e4d0c20a40e0b96ed74622c3c1
vladsopot@mail.ru <vladsopot@mail.ru>
2023-08-18 16:06:51 +0300

    refactor: resolve conflicts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,3 @@
-
 import './app.css';
 import RegView from '../view/registration/reg-view';
 import LogInView from '../view/log-in/log-in_view';
@@ -9,31 +8,31 @@ import FooterView from '../view/footer/footer-view';
 export default class App {
   constructor() {
     // this.createRegView();
-    this.createLogInView();
-    // this.createView();
+    // this.createLogInView();
+    this.createView();
   }
-  createLogInView() {
-    const header = new HeaderView();
-    const logIn = new LogInView();
-    const footer = new FooterView();
 
-    document.body.append(/* header.getHtmlElement(),*/ logIn.getHtmlElement()/*, footer.getHtmlElement()*/);
-  }
+  // createLogInView() {
+  //   const header = new HeaderView();
+  //   const logIn = new LogInView();
+  //   const footer = new FooterView();
+
+  //   document.body.append(header.getHtmlElement(), logIn.getHtmlElement(), footer.getHtmlElement());
+  // }
 
   // createRegView() {
   //   const header = new HeaderView();
   //   const registr = new RegView();
   //   const footer = new FooterView();
 
-  //   document.body.append(/*header.getHtmlElement(),*/ registr.getHtmlElement()/*, footer.getHtmlElement()*/);
+  //   document.body.append(header.getHtmlElement(), registr.getHtmlElement(), footer.getHtmlElement());
   // }
-  }
 
-//   createView() {
-//     const header = new HeaderView();
-//     const main = new MainView();
-//     const footer = new FooterView();
+    createView() {
+      const header = new HeaderView();
+      const main = new MainView();
+      const footer = new FooterView();
 
-//     document.body.append(header.getHtmlElement(), main.getHtmlElement(), footer.getHtmlElement());
-//   }
+      document.body.append(header.getHtmlElement(), main.getHtmlElement(), footer.getHtmlElement());
+    }
 }
