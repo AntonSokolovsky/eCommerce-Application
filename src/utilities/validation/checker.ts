9be910a7c1d11221f733443b8dbf0826a4a4b67a
vladsopot@mail.ru <vladsopot@mail.ru>
2023-08-21 22:40:59 +0300

    feat: add adress, surname etc

--- src/utilities/validation/checker.ts
+++ src/utilities/validation/checker.ts
@@ -3,20 +3,16 @@ import formValidation from '../validation/vlidationForm';
 export default function check(rules: HTMLElement, regExpression: RegExp, value: string, Input: HTMLElement, passwordToCheck?: string) {
   if (passwordToCheck) {
     if (regExpression.test(value) && value === passwordToCheck) {
-      console.log('Good');
       if (!Input.classList.contains('accept')) Input.classList.add('accept');
       rules.style.display = 'none';
     } else {
-      console.log('Bad');
       if (Input.classList.contains('accept')) Input.classList.remove('accept');
     }
   } else {
     if (regExpression.test(value)) {
-      console.log('Good');
       if (!Input.classList.contains('accept')) Input.classList.add('accept');
       rules.style.display = 'none';
     } else {
-      console.log('Bad');
       if (Input.classList.contains('accept')) Input.classList.remove('accept');
     }
   }
