486940240bbfb0f57eab3278310d2e3c10bb6f59
AntonSokolovsky <anton.a.sokolovsky@gmail.com>
2023-09-03 13:13:49 +0300

    fix: resolve warnings

--- src/utilities/is-user-login.ts
+++ src/utilities/is-user-login.ts
@@ -1,6 +1,5 @@
 import { TokenNames } from '../type/enum-token';
 
 export function isUserLogin() {
-  console.log(localStorage.getItem(TokenNames.TOKEN_CUSTOMER) ? true : false);
   return localStorage.getItem(TokenNames.TOKEN_CUSTOMER) ? true : false;
 }
